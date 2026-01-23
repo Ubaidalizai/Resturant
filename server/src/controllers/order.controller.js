8f946376df333f1617bdde51d2685d9c2ff8a334
Ansar-stack <ansar.stack@gmail.com>
2026-01-24 02:52:52 +0430

    fix: Fix the handle Login

--- server/src/controllers/order.controller.js
+++ server/src/controllers/order.controller.js
@@ -1,1 +1,31 @@
+import { asyncHandler } from "../utils/asyncHandler.util.js";
+import { Order } from "../models/order.model.js";
+
+// Add the order 
+export const addOrder = asyncHandler(async (req, res)=>{
+    const {userId, items, quantity, amount, tableId} = req.body;
+    const newOrder = await Order.create({userId, items, quantity, amount, tableId});
+    res.respond(201, "Order placed successfully", newOrder);
+});
+// Get all orders
+export const getOrders = asyncHandler(async (req, res)=>{
+    const orders = await Order.find({}).populate('userId').populate('tableId').populate('items.foodId');
+    res.respond(200, "Orders fetched successfully", orders);
+});
+
+// Get single order 
+export const getOrder = asyncHandler(async (req, res)=>{
+    const {orderId} = req.params;
+    const order = await Order.findById(orderId).populate('userId').populate('tableId').populate('items.foodId');
+    res.respond(200, "Order fetched successfully", order);
+});
+
+// Delete order
+export const deleteOrder = asyncHandler(async (req, res)=>{
+    const {orderId} = req.params;
+    await Order.findByIdAndDelete(orderId);
+    res.respond(200, "Order deleted successfully");
+});
+
+
 
