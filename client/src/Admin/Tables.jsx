e9ae9e1c245000246f4fbaf180ee5563ecb6cf31
Nabiullah Hayat <nabiullahhayat.786@gmail.com>
2026-03-03 10:06:55 +0430

    Describe your changes here

--- client/src/Admin/Tables.jsx
+++ client/src/Admin/Tables.jsx
@@ -6,7 +6,7 @@ function Tables() {
   const [tables, setTables] = useState([]);
   const [selectedTable, setSelectedTable] = useState(null);
 
-  //Fetch All added tables from database
+  //Fetch All added tables from database3
   useEffect(() => {
     const fetchTables = async () => {
       try {
